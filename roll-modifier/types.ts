00402cdcaf9813977626eb9e8c1b8fbf1b2255f7
sinjin25 <raionus2@gmail.com>
2026-07-09 03:04:49 -0700

    create util for finding roll mods for tests

--- roll-modifier/types.ts
+++ roll-modifier/types.ts
@@ -22,3 +22,16 @@ export type RollModifierFunc = () => RollModifierResult
 export type RollModifierFuncFactory = (
     data: RollModifierRequiredData,
 ) => RollModifierFunc
+
+export const util_findRollModifierGroupItem = (
+    rollModifierResult: RollModifierResult,
+    query: {
+        groupName: 'equipment mod' | 'base mod' | 'status mod' | 'feat mod', modDisplayName: string
+    }
+) => {
+    if (!query.groupName || !query.modDisplayName) throw Error('did not pass either a query.groupName or query.modDisplayName')
+    const group = rollModifierResult.groups.find(a => a.displayName === query.groupName)
+    if (!group) return undefined
+    const member = group.entries.find(a => a.displayName === query.modDisplayName)
+    return member
+}

a7a11080abdaa6b3ec2446a3d3a66ff018717fe6
sinjin25 <raionus2@gmail.com>
2026-07-29 23:37:01 -0700

    #77

--- log2/terminal/crit-threat-range.ts
+++ log2/terminal/crit-threat-range.ts
@@ -1,6 +1,5 @@
 import newModNode, { leaf, sumFunc } from "..";
-import { EveryTree, OwnerMaximal } from "../types";
-import { equipmentIsWeapon } from "../../equipment-sheet";
+import { EveryTree, OwnerMaximal, TreeSubproblems } from "../types";
 import critThreatRangeMod from "../composition/crit-threat-range-mod";
 
 const displayName: EveryTree = 'crit-threat-range'
@@ -8,10 +7,16 @@ const DEFAULT_THREAT_RANGE = 20
 export default (owner: OwnerMaximal) => {
     const relevantSlot = owner.relevantSlot
     if (!relevantSlot) throw Error('Need to pass in a weapon to relevantSlot')
-    if (!equipmentIsWeapon(relevantSlot)) throw Error('Need to pass in a weapon to relevantSlot')
 
-    return newModNode(displayName, [
-        leaf('weapon base', relevantSlot.critRange ?? DEFAULT_THREAT_RANGE),
-        critThreatRangeMod(owner),
-    ], sumFunc)
+    const weaponCritThreatHandler = relevantSlot.broadContexts['crit-threat-range']
+
+    const subproblems: TreeSubproblems = {
+        'crit-threat-range-mod': critThreatRangeMod(owner),
+        'crit-threat-range': weaponCritThreatHandler ? weaponCritThreatHandler(owner) : leaf(relevantSlot.displayName, DEFAULT_THREAT_RANGE)
+    }
+
+    const subpr = Object.values(subproblems)
+        .filter(a => !!a)
+
+    return newModNode(displayName, subpr, sumFunc)
 }
